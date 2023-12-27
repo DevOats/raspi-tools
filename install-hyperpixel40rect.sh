cee7046e7ad0ff3cac1c8df6b9fde6f047df4f69
Joost Haverkort <1299167+joostit@users.noreply.github.com>
2023-12-27 13:55:15 +0100

    Trying to work around the sudo problem in install-hyperpixel40rect.sh

--- install-hyperpixel40rect.sh
+++ install-hyperpixel40rect.sh
@@ -3,37 +3,37 @@
 # Author Joost Haverkort 
 # https://github.com/DevOats
 
-if [[ $(/usr/bin/id -u) -ne 0 ]]; then
-    echo "Script needs to be executed with Sudo"
-    exit
-fi
+#if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+#    echo "Script needs to be executed with Sudo"
+#    exit
+#fi
 
 
 # Disable I2C
 echo "Disabling I2C"
-raspi-config nonint do_i2c 1
+sudo raspi-config nonint do_i2c 1
 
 #disable SPI
 echo "Disabling SPI"
-raspi-config nonint do_spi 1
+sudo raspi-config nonint do_spi 1
 
 #disable Single wire
 echo "Disabling OneWire"
-raspi-config nonint do_onewire 1
+sudo raspi-config nonint do_onewire 1
 
 # Disable UART pin
 echo "Disabling UART HW"
-raspi-config nonint do_serial_hw 1
+sudo raspi-config nonint do_serial_hw 1
 
 # serial console
 echo "Disabling Serial Console"
-raspi-config nonint do_serial_cons 1
+sudo raspi-config nonint do_serial_cons 1
 
 echo "Prepending HyperPixel 4.0 config to /boot/config.txt:"
-sed '# For HyperPixel 4.0:' -i /boot/config.txt
-sed 'dtoverlay=vc4-kms-dpi-hyperpixel4' -i /boot/config.txt
-sed 'dtparam=rotate=270,touchscreen-swapped-x-y,touchscreen-inverted-y' -i /boot/config.txt
-sed '' -i /boot/config.txt
+sudo sed '# For HyperPixel 4.0:' -i /boot/config.txt
+sudo sed 'dtoverlay=vc4-kms-dpi-hyperpixel4' -i /boot/config.txt
+sudo sed 'dtparam=rotate=270,touchscreen-swapped-x-y,touchscreen-inverted-y' -i /boot/config.txt
+sudo sed '' -i /boot/config.txt
 
 
 echo "Adding HyperPixel 4.0 config to the WayFire config file:"
