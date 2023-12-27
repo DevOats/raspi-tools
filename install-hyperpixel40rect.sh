280b571144da91877efd3e7327517af9f1f8c217
Joost Haverkort <1299167+joostit@users.noreply.github.com>
2023-12-27 13:03:33 +0100

    Set all interfaces to be disabled in install-hyperpixel40rect.sh

--- install-hyperpixel40rect.sh
+++ install-hyperpixel40rect.sh
@@ -7,12 +7,14 @@
 # Disable I2C
 raspi-config nonint do_i2c 1
 
-
 #disable SPI
+raspi-config nonint do_spi 1
 
 #disable Single wire
+raspi-config nonint do_onewire 1
 
 # Disable UART pin
+raspi-config nonint do_serial_hw 1
 
 # serial console
-sudo rpi-serial-console disable
+raspi-config nonint do_serial_cons 1
